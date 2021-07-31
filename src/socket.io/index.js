54d04a61515f917dc7033f6c7d0e7a07299640c3
leevyx <lnhv.26112000@gmail.com>
2021-07-31 15:29:15 +0700

    update serversocket

--- src/socket.io/index.js
+++ src/socket.io/index.js
@@ -1,13 +1,16 @@
 export function Socket(io) {
   io.on("connection", (socket) => {
-    console.log("User connect id: ", socket.id);
-    
-    socket.on("sendMessage", (data, room) => {
-      socket.to(room).emit("receiveMessage", data);
+    // console.log("user connect: ", socket.id);
+    socket.on("sendMessage", (data, room, userId) => {
+      socket.to(room).emit("receiveMessage", data, userId);
     });
 
     socket.on("join-room", (room) => {
       socket.join(room);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("user disconnect");
     });
   });
 }
